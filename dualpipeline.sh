63abb96ee84ad2cba3ef04074911e27752dbee74
Digital1O1 <Christopher.Ta1125@gmail.com>
2024-12-04 15:31:07 -0600

    backing up

--- dualpipeline.sh
+++ dualpipeline.sh
@@ -4,8 +4,8 @@ pkill raspivid
 pkill gst-launch-1.0
 export DISPLAY=:0
 
-WIDTH=640
-HEIGHT=480
+# Send to Linux Laptop
+# Put Laptop IP address here
 
 # irCamera
 raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 0 | \
@@ -17,15 +17,7 @@ raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 1 | \
 gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
 udpsink host=172.17.141.30 port=5001 &
 
-#gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=1920,height=1080,framerate=30/1,format=NV12 ! \
-#v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! 'video/x-h264,level=(string)4.1,profile=(string)baseline' \
-#! h264parse ! rtph264pay ! udpsink host=172.17.141.30 port=5000 &
-
-#gst-launch-1.0 v4l2src device=/dev/video1 ! video/x-raw,width=1920,height=1080,framerate=30/1,format=NV12 ! \
-#v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! 'video/x-h264,level=(string)4.1,profile=(string)baseline' \
-#! h264parse ! rtph264pay ! udpsink host=172.17.141.30 port=5001 &
-
-
+# Place RPI IP address here
 # Command to receive the stream
 # Streaming PI : 172.17.141.124
 # irCameraPipeline
--- singlepipeline.sh
+++ singlepipeline.sh
@@ -3,17 +3,17 @@ pkill gst-launch-1.0
 
 export DISPLAY=:0
 
-# OLD
-#gst-launch-1.0 -vvvvv v4l2src device=/dev/video1 ! \
-#video/x-raw,width=1920,height=1080,framerate=30/1,format=I420,colorimetry=bt601,interlace-mode=progressive \
-#! v4l2h264enc ! video/x-h264,profile=baseline ! h264parse ! rtph264pay config-interval=-1 \
-#! udpsink host=172.17.141.30 port=5000
+# Send to Linux Laptop
+# Put Laptop IP address here
+# raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 1 | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=172.17.140.56 port=5000 &
 
-# CURRENTLY TESTING
-gst-launch-1.0 -vvvvv v4l2src device=/dev/video1 \
-! video/x-raw,width=3280,height=2464,framerate=30/1,format=I420,colorimetry=bt601,interlace-mode=progressive \
-! x264enc ! video/x-h264,profile=baseline ! h264parse ! rtph264pay config-interval=-1 \
-! udpsink host=172.17.141.30 port=5001 &
+raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 1 | gst-launch-1.0  fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=172.17.140.56 port=5000 &
 
-#gst-launch-1.0 -v udpsrc address=172.17.141.30 port=5003 caps=application/x-rtp ! \
-#rtph264depay ! h264parse ! queue ! v4l2h264dec ! autovideosink sync=false
+# Pipeline to recieve frames from laptop
+# Place RPI IP address here
+gst-launch-1.0 -v udpsrc address=172.17.141.124 port=5001 caps=application/x-rtp ! \
+rtph264depay ! h264parse ! queue ! v4l2h264dec ! autovideosink sync=false
+
+
+
+
