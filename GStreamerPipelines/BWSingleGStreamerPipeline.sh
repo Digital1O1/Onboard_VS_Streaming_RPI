9ab43fed834b7836f05874b3c92c5a682382fca6
Digital1O1 <Christopher.Ta1125@gmail.com>
2024-12-09 14:44:05 -0600

    libcamerasrc works

--- GStreamerPipelines/BWSingleGStreamerPipeline.sh
+++ GStreamerPipelines/BWSingleGStreamerPipeline.sh
@@ -9,7 +9,7 @@ export DISPLAY=:0
 # Put Laptop IP address here
 # raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 1 | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=172.17.140.56 port=5000 &
 
-raspivid -t 0 -n -w 640 -h 480 -fps 30 -b 2000000 -o - -cs 1 | gst-launch-1.0  fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=172.17.140.56 port=5001 &
+ gst-launch-1.0 libcamerasrc camera-name="/base/soc/i2c0mux/i2c@0/imx219@10" ! capsfilter caps=video/x-raw,width=640,height=480,format=NV12 ! v4l2convert ! queue ! v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! 'video/x-h264,level=(string)4.2,profile=(string)baseline' ! h264parse ! rtph264pay ! queue ! udpsink host=172.17.140.56 port=5001 &
 
 # Pipeline to recieve frames from laptop
 # Place RPI IP address here
