1a3559afa4a91835e982fd2725fa745ad86c271a
Digital1O1 <Christopher.Ta1125@gmail.com>
2025-01-17 12:46:04 -0600

    working on gathering latency data

--- GStreamer Folder/GStreamerPipelines_BW/TCP-IP_Folder_RPI/TestScripts/TestPipeline.sh
+++ GStreamer Folder/GStreamerPipelines_BW/TCP-IP_Folder_RPI/TestScripts/TestPipeline.sh
@@ -13,7 +13,7 @@ pkill gst-launch-1.0
 # tcpserversink host=0.0.0.0 port=5000
 
 # Send to laptop
-gst-launch-1.0 libcamerasrc camera-name="/base/soc/i2c0mux/i2c@0/imx219@10" ! capsfilter caps=video/x-raw,width=640,height=480,format=NV12 ! v4l2convert ! queue ! v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! 'video/x-h264,level=(string)4.2,profile=(string)baseline' ! tcpserversink host=0.0.0.0 port=5000 &
+gst-launch-1.0 libcamerasrc camera-name="/base/soc/i2c0mux/i2c@0/imx219@10" ! capsfilter caps=video/x-raw,width=640,height=480,format=NV12 ! v4l2convert ! queue ! v4l2h264enc extra-controls="controls,repeat_sequence_header=1" ! 'video/x-h264,level=(string)4.2,profile=(string)baseline' ! tcpserversink host=172.17.140.24 port=7001 &
 
 # Recieve from laptop
 
